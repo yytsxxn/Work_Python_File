0e06a9139d5fcaf1f1aa271345b10341a5c428d9
yytsxxn <1753337170@qq.com>
2020-10-03 14:29:07 +0800

    10.3

--- global用法.py
+++ global用法.py
@@ -4,11 +4,12 @@ print(a,b)
 print('---------')
 def f1():
     global a
-    #print(a,b)
-    #print('--------')
-    a = 3
-    b = 4
     print(a,b)
+    print('--------')
+    a = 3
+    #b = 4
+    c = a+b
+    print(a,b,c)
     print('-------')
 f1()
 print(a,b)
--- 工时.py
+++ 工时.py
@@ -2,13 +2,13 @@ import math
 # 工时计算
 def estimated_time(size,number):
     time = size*80/number
-    return time
+    print('项目大小为1.5个标准项目，使用2个人力完成，需要工时数为：'+str(time))
 
 # 人力计算
 def estimated_number(size,time):
     number = math.ceil(size*80/time)
     return number
-print('项目大小为1.5个标准项目，使用2个人力完成，需要工时数为：'+str(estimated_time(1.5,2)))
+
 print('项目大小为0.5个标准项目，如果需要在20.0个工时完成，则需要人力数为：'+str(estimated_number(0.5,20)))
 
 # 调用工时计算函数
